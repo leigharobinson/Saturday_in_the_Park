243c3c8e373c116ba3b4964e180f005778bc023b
leigharobinson <lmarobinson24@gmail.com>
2020-08-18 21:11:19 -0500

    Identifying sections of React and added console.log()

--- src/components/home/Area.js
+++ src/components/home/Area.js
@@ -1,17 +1,19 @@
-import React from "react"
+import React from "react";
 
-const Area = props => {
+const Area = (props) => {
+  return (
+    <>
+      Console.log("FOUND THE LOOPING LIST of PARKS")
+      <section className="parkArea">
+        <button
+          className="fakeLink parkArea__link"
+          onClick={() => props.getAttractions(props.area.id)}
+        >
+          {props.area.name}
+        </button>
+      </section>
+    </>
+  );
+};
 
-    return (
-        <>
-            <section className="parkArea">
-                <button className="fakeLink parkArea__link"
-                        onClick={() => props.getAttractions(props.area.id)}>
-                    {props.area.name}
-                </button>
-            </section>
-        </>
-    )
-}
-
-export default Area
+export default Area;
